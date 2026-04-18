e4db5a69112eb6ceacf8cfe43aaf0a66147a245b
Romain <romain.hannoir@laposte.net>
2026-04-18 14:33:06 +0200

    add var env update in cicd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,12 @@
-import {commonEnvironment} from "./commonEnvironment";
-
 export const environment = {
-  ...commonEnvironment,
+  production: false,
   strava: {
-    ...commonEnvironment.strava
+    clientId: "#{CLIENT_ID}#",
+    refreshToken: '#{REFRESH_TOKEN}#',
+    baseUrl: 'https://www.strava.com/api/v3',
+    authUrl: 'https://www.strava.com/oauth/mobile/authorize',
+    perPage: 100
   },
-  production: true,
+  redirectUrl: 'http://localhost:4200/strava-auth-callback',
+  githubApiUrl: 'https://api.github.com/repos/Romain-02/SportTracker/releases/latest'
 };
